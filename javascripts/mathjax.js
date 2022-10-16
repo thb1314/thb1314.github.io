ab264dc204cb15f9dac8dc6896ff2ab2f4744bff
thb1314 <13953062665@163.com>
2022-10-16 19:22:17 +0800

    增加convnext

---
+++ javascripts/mathjax.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['$ ', ' $'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  "HTML-CSS": { fonts: ["TeX"], linebreaks: { automatic: true } },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+
